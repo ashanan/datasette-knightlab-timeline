fc762d632c9006ad9d1701ecaafb46c12632d90f
Avner Shanan <avner.shanan@devbootcamp.com>
2022-06-02 22:54:04 -0500

    Register timeline URL with hello world view

--- datasette-knightlab-timeline.py
+++ datasette-knightlab-timeline.py
@@ -1,4 +1,4 @@
-from datasette import hookimpl
+from datasette import hookimpl, Response
 
 @hookimpl
 def extra_body_script():
@@ -6,3 +6,18 @@ def extra_body_script():
         "module": True,
         "script": "console.log('hello datasette world!')"
     }
+
+@hookimpl
+def menu_links(datasette):
+    return [
+        {"href": datasette.urls.path("/-/timeline"), "label": "Timeline"}
+    ]
+
+@hookimpl
+def register_routes():
+    return [
+        (r"^/-/timeline$", timeline)
+    ]
+
+async def timeline(request):
+    return Response.html("Hello world")
