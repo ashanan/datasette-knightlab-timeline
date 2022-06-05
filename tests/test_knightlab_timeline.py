dbcced0c6b0c86f20a9f8e2d4ad78e818aa1a401
Avner Shanan <avner.shanan@devbootcamp.com>
2022-06-05 14:11:10 -0500

    Add test for plugin route

--- tests/test_knightlab_timeline.py
+++ tests/test_knightlab_timeline.py
@@ -9,3 +9,10 @@ async def test_plugin_is_installed():
     assert response.status_code == 200
     installed_plugins = {p["name"] for p in response.json()}
     assert "datasette-knightlab-timeline" in installed_plugins
+
+@pytest.mark.asyncio
+async def test_timeline_route_reachable():
+    datasette = Datasette(memory=True)
+    response = await datasette.client.get("/-/timeline")
+    assert response.status_code == 200
+    assert 'id=\'timeline-embed\'' in response.text
