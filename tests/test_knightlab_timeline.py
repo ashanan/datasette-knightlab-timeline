bb1c032c90454dc4594229043415d7c1fa330528
Avner Shanan <avner.shanan@devbootcamp.com>
2022-06-12 13:41:22 -0500

    Add test fixture with data and JSON endpoint test

--- tests/test_knightlab_timeline.py
+++ tests/test_knightlab_timeline.py
@@ -1,6 +1,10 @@
 from datasette.app import Datasette
-import pytest
 
+from datetime import datetime, timedelta
+import pytest
+import sqlite_utils
+import textwrap
+import yaml
 
 @pytest.mark.asyncio
 async def test_plugin_is_installed():
@@ -16,3 +20,48 @@ async def test_timeline_route_reachable():
     response = await datasette.client.get("/-/timeline")
     assert response.status_code == 200
     assert 'id=\'timeline-embed\'' in response.text
+
+@pytest.mark.asyncio
+async def test_timeline_json_reachable(ds):
+    response = await ds.client.get("/-/timeline.json")
+    assert response.status_code == 200
+
+    payload = response.json()
+    assert len(payload['events']) > 0
+
+@pytest.fixture
+def ds(tmp_path_factory):
+    db_path = tmp_path_factory.mktemp("dbs") / "timeline-test.db"
+    database = sqlite_utils.Database(db_path)
+
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+
+    # Create a simple timeline table for test data
+    database["timeline"].insert_all([{
+        "id": 1,
+        "start_date": today.isoformat(),
+        "text": "High temp: ",
+        "text_column": "89°F"
+    }, {
+        "id": 2,
+        "start_date": yesterday.isoformat(),
+        "text": "High temp: ",
+        "text_column": "95°F"
+    }])
+
+    return Datasette(
+        [str(db_path)],
+        metadata=METADATA,
+    )
+
+METADATA = yaml.safe_load(textwrap.dedent(
+        """
+    plugins:
+        datasette-knightlab-timeline:
+            database: timeline-test
+            query: SELECT * FROM timeline;
+            text: text-goes-here
+            text_column: text_column
+    """
+    ))
