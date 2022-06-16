90cbee6269189751be111fc5ee18300f8ca36c11
Avner Shanan <avner.shanan@devbootcamp.com>
2022-06-15 21:10:18 -0500

    Store slide text as an array

--- tests/test_timeline_slide.py
+++ tests/test_timeline_slide.py
@@ -4,7 +4,17 @@ import pytest
 from datasette_knightlab_timeline.timeline_slide import TimelineSlide
 
 @pytest.mark.asyncio
-async def test_set_start_date():
+async def test_set_start_date(text):
     today = datetime.now()
-    slide = TimelineSlide({'start_date': today, 'text': ''})
+    slide = TimelineSlide({'start_date': today, 'text': text})
     assert slide.start_date == today
+
+@pytest.mark.asyncio
+async def test_set_text(text):
+    today = datetime.now()
+    slide = TimelineSlide({'start_date': today, 'text': text})
+    assert slide.text == [text]
+
+@pytest.fixture
+def text():
+    return 'test'
