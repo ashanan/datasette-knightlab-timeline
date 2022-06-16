586a1ea7e08ccb4ced424a62ff788215a8e3e9c0
Avner Shanan <avner.shanan@devbootcamp.com>
2022-06-16 11:02:34 -0500

    Use more fixtures

--- tests/test_timeline_slide.py
+++ tests/test_timeline_slide.py
@@ -4,21 +4,15 @@ import pytest
 from datasette_knightlab_timeline.timeline_slide import TimelineSlide
 
 @pytest.mark.asyncio
-async def test_set_start_date(text):
-    today = datetime.now()
-    slide = TimelineSlide({'start_date': today, 'text': text})
+async def test_set_start_date(today, slide):
     assert slide.start_date == today
 
 @pytest.mark.asyncio
-async def test_set_text(text):
-    today = datetime.now()
-    slide = TimelineSlide({'start_date': today, 'text': text})
+async def test_set_text(text, slide):
     assert slide.text == [text]
 
 @pytest.mark.asyncio
-async def test_add_text(text):
-    today = datetime.now()
-    slide = TimelineSlide({'start_date': today, 'text': text})
+async def test_add_text(text, slide):
     addedText = "test text added in test_add_text"
     slide.addText(addedText)
     assert slide.text == [text, addedText]
@@ -26,3 +20,11 @@ async def test_add_text(text):
 @pytest.fixture
 def text():
     return 'test'
+
+@pytest.fixture
+def today():
+    return datetime.now()
+
+@pytest.fixture
+def slide(today, text):
+    return TimelineSlide({'start_date': today, 'text': text})
